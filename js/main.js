d27defe1659a25c5c21ff11bd81f101bb163b7ed
muhammadmohie98 <muhammadmohie98@gmail.com>
2021-08-27 19:36:59 +0200

    Update fetching from the api

--- js/main.js
+++ js/main.js
@@ -11,192 +11,193 @@ const question = document.querySelector(".question");
 const answers = document.querySelector(".answers");
 const previous = document.querySelector(".previous");
 const next = document.querySelector(".next");
-const url = './Questions.json';
 let proceed = document.getElementById("proceed");
-let loadedQuizzes;
+let quizzes;
 const answer = document.querySelector(".answer");
-(async function fetchQuizzes(){
+(async function fetchQuizzes() {
   try {
+    const url = 'http://127.0.0.1:3000/quizzes';
     let response = await fetch(url);
-    let json = await response.json();
-    loadedQuizzes = json;
+    quizzes = await response.json();
+
+    let premadeQuizzes = document.querySelector('.premade-quizzes')
+    for (let i = 0; i < quizzes.length; i++) {
+      let quiz = document.createElement('button');
+      quiz.textContent = quizzes[i];
+      quiz.classList.add('quizzes');
+      premadeQuizzes.appendChild(quiz);
+    }
+
     // Add code here
-    let q=0; // the current question
-    let c = 0 ; // the current answer
-    let results= [];
-     score = 0 ;
-    previous.disabled=true;          // disable the previous button in the first question
+    let q = 0; // the current question
+    let c = 0; // the current answer
+    let results = [];
+    score = 0;
+    // previous.disabled = true;          // disable the previous button in the first question
 
-//======================================================================================
+    //======================================================================================
 
-mainContainer.addEventListener("click", (event) => {
-  if (event.target.tagName === "BUTTON") {
-    requiredQuiz = event.target.textContent;
-    mainContainer.style.display = "none";
-    confirmation.style.display = "block";
-  }
-});
+    mainContainer.addEventListener("click", (event) => {
+      if (event.target.tagName === "BUTTON") {
+        requiredQuiz = event.target.textContent;
+        mainContainer.style.display = "none";
+        confirmation.style.display = "block";
+        loadRequiredQuiz();
+      }
+    });
+    async function loadRequiredQuiz(){
+      let res = await fetch(`http://127.0.0.1:3000/quizzes/${requiredQuiz}`);
+      loadedQuiz = await res.json();
+    }
 
+    confirmation.addEventListener("click", (event) => {
+      if (event.target.tagName === "BUTTON") {
+        if (event.target.id === "change") {
+          mainContainer.style.display = "block";
+          confirmation.style.display = "none";
+        } else {
+          startQuiz(requiredQuiz);
+        }
+      }
+    });
 
-
-confirmation.addEventListener("click", (event) => {
-  if (event.target.tagName === "BUTTON") {
-    if (event.target.id === "change") {
-      mainContainer.style.display = "block";
+    function startQuiz(quizName) {
+      mainContainer.style.display = "none";
       confirmation.style.display = "none";
-    } else {
-      startQuiz(requiredQuiz);
+      quizContainer.style.display = "block";
+      const quizTitle = document.querySelector('.quiz-title');
+      console.log(quizName);
+      quizTitle.innerText = `${quizName.toUpperCase()} quiz.`;
+      timer(timeCounter);
     }
-  }
-});
 
-function startQuiz(quiz) {
-  mainContainer.style.display = "none";
-  confirmation.style.display = "none";
-  quizContainer.style.display = "block";
-  timer(timeCounter);
-}
+    //============================== Timer function =================================================
+    function timer(timeCounter) {
+      const countdown = setInterval(() => {
+        timeCounter--;
+        printTime(timeCounter);
+        if (timeCounter < 1) {
+          clearInterval(countdown);
+          time.setAttribute('id', '00')
+          finshed();
+        }
+      }, 1000);
 
-//============================== Timer function =================================================
-function timer(timeCounter) {
-  const countdown = setInterval(() => {
-    timeCounter--;
-    printTime(timeCounter);
-    if (timeCounter < 1) {
-      clearInterval(countdown);
-      time.setAttribute('id','00')
-      finshed();
-    }
-  }, 1000);
+      function printTime(seconds) {
+        let min = Math.floor(seconds / 60);
+        let sec = Math.floor(seconds % 60);
+        time.textContent = `${min < 10 ? "0" : ""}${min}:${sec < 10 ? "0" : ""}${sec}`;
+      }
+      function finshed() {
+        quizContainer.style.display = "none";
+        resultContainer.style.display = 'block'
+        // Display quiz result
+        mainContainer.style.display = "none";
 
-  function printTime(seconds) {
-    let min = Math.floor(seconds / 60);
-    let sec = Math.floor(seconds % 60);
-    time.textContent = `${min < 10 ? "0" : ""}${min}:${sec < 10 ? "0" : ""}${sec}`;
-  }
-  function finshed() {
-    quizContainer.style.display = "none";
-    resultContainer.style.display = 'block'
-    // Display quiz result
-    mainContainer.style.display = "none";
-        
-    for(let i = 0; i<results.length;i++){          // calculat the sore if the timer finshed
-      if(results[i]==loadedQuiz.correct[i])
+        for (let i = 0; i < results.length; i++) {          // calculat the sore if the timer finshed
+          if (results[i] == loadedQuiz.correct[i])
             score++;
- }
-    console.log(loadedQuiz.correct);  
-    console.log(results);
-    console.log(score);
-  }
-}
+        }
+        // console.log(loadedQuiz.correct);
+        // console.log(results);
+        // console.log(score);
+      }
+    }
 
-//==============================================================================
-    proceed.addEventListener("click", () =>
-    {
-      next.disabled = true;
-      for (let i=0; i<loadedQuizzes.length; i++)
-        if (requiredQuiz === loadedQuizzes[i].title)
-          loadedQuiz = loadedQuizzes[i];    
-      question.textContent = loadedQuiz.questions[0];
-      for (let i = 0; i < loadedQuiz.answers[0].length; i++)
-      {
+    //==============================================================================
+    proceed.addEventListener("click", () => {
+      // next.disabled = true;
+      question.textContent = `Q${q+1}: ${loadedQuiz.questions[0].title}`;
+      for (let i = 0; i < loadedQuiz.questions[0].answers.length; i++) {
         let ansr = document.createElement('div');
         ansr.classList.add('answer');
         ansr.setAttribute('id', `${i}`);
-        ansr.textContent = loadedQuiz.answers[0][i];
+        ansr.textContent = loadedQuiz.questions[0].answers[i];
         answers.appendChild(ansr);
 
-        answers.children.item(i).addEventListener("click",()=>{    // save the correct answer
+        answers.children.item(i).addEventListener("click", () => {    // save the correct answer
           c = i;
-          next.disabled= false;
+          // next.disabled = false;
         })
-       
+
       }
       results.push(c);                                           // push the correct anwser in the array
       results.shift();
     });
 
     next.addEventListener('click', () => {
-      
-    next.disabled=true;
-    previous.disabled=false;
 
-      if(q === loadedQuiz.questions.length-2)
-      {
-        next.textContent = 'Finish attempt'
-       
+      // next.disabled = true;
+      // previous.disabled = false;
+
+      if (q === loadedQuiz.questions.length - 2) {
+        next.textContent = 'Finish attempt';
       }
-      if(q === loadedQuiz.questions.length-1)
-      {
+      if (q === loadedQuiz.questions.length - 1) {
         // show results from here
         quizContainer.style.display = "none";
         resultContainer.style.display = 'block';
-       
-      }
 
+      }
       answers.innerHTML = '';
-      question.textContent = loadedQuiz.questions[++q];
-      if(q<=4){
-      for (let i = 0; i < loadedQuiz.answers[q].length; i++)
-      {
+      q++;
+      question.textContent = `Q${q+1}: ${loadedQuiz.questions[q].title}`;
+      for (let i = 0; i < loadedQuiz.questions[0].answers.length; i++) {
         let ansr = document.createElement('div');
         ansr.classList.add('answer');
         ansr.setAttribute('id', `${i}`);
-        ansr.textContent = loadedQuiz.answers[q][i];
+        ansr.textContent = loadedQuiz.questions[q].answers[i];
         answers.appendChild(ansr);
 
-        answers.children.item(i).addEventListener("click",()=>{     // save trhe correct answers
-          
-            c = i;
-            next.disabled=false;
-          
+        answers.children.item(i).addEventListener("click", () => {     // save trhe correct answers
+
+          c = i;
+          // next.disabled = false;
+
         })
-        
-      }
-      
       }
       results.push(c);
 
-      if(q==loadedQuiz.questions.length ){                     // calculat the total socre 
-        console.log(loadedQuiz.correct);  
+      if (q == loadedQuiz.questions.length) {                     // calculat the total socre 
+        console.log(loadedQuiz.correct);
         console.log(results);
-        for(let i = 0; i<results.length;i++){
-          if(results[i]==loadedQuiz.correct[i])
-                score++;
+        for (let i = 0; i < results.length; i++) {
+          if (results[i] == loadedQuiz.correct[i])
+            score++;
         }
-        console.log(score);
+        // console.log(score);
       }
 
     });
 
     previous.addEventListener('click', () => {
 
-      if(q < loadedQuiz.questions.length)    // change the name of the button
+      if (q < loadedQuiz.questions.length)    // change the name of the button
       {
         next.textContent = 'Next'
       }
-    
-      if(q === 0)
-          return;
-      
+
+      if (q === 0)
+        return;
+
       answers.innerHTML = '';
-      question.textContent = loadedQuiz.questions[--q];
-      for (let i = 0; i < loadedQuiz.answers[q].length; i++)
-      {
+      q--;
+      question.textContent =`Q${q+1}: ${loadedQuiz.questions[q].title}`;
+      for (let i = 0; i < loadedQuiz.questions[0].answers.length; i++) {
         let ansr = document.createElement('div');
         ansr.classList.add('answer');
         ansr.setAttribute('id', `${i}`);
-        ansr.textContent = loadedQuiz.answers[q][i];
+        ansr.textContent = loadedQuiz.questions[q].answers[i];
         answers.appendChild(ansr);
 
-        answers.children.item(i).addEventListener("click",()=>{
-          c=i;
-          next.disabled=false;
+        answers.children.item(i).addEventListener("click", () => {
+          c = i;
+          next.disabled = false;
         })
 
       }
       results.pop(); // remove the answer from the array
-       
+
     });
 
   } catch (error) {
