9036fcb9e6e3e7fb5bd8d5bd2db6a8a99baa92e3
Adham Salama <adhamsalama@zohomail.com>
2021-08-29 14:36:06 +0200

    Add print questions, print answer, save quiz buttons and show the container only when the user click on a quiz

--- js/manage-quiz.js
+++ js/manage-quiz.js
@@ -10,14 +10,43 @@ let questions = [];
         let quizName = btn.innerText
         let quiz = await fetch(`http://127.0.0.1:3000/quizzes/${quizName}`).then(res => res.json())
         questions = quiz.questions
-        console.log(questions)
+        document.querySelectorAll('.hide').forEach(div => div.style.visibility = "visible")
         let quizQuestions = document.querySelector('.questions')
         quizQuestions.innerHTML = ''
         quiz.questions.forEach(question => {
             quizQuestions.innerHTML += createQuestion(question.title, question.answers, question["correct-answer"])
         })
+        // Add button click events
+        
+        document.querySelector('#save-quiz').onclick = () => {
+            postData('http://127.0.0.1:3000/quizzes', {
+                name: quizName,
+                questions: questions
+            })
+            .then(response => {
+                if (response.ok)
+                    alert('Quiz saved successfully.')
+                else {
+                    alert('Something went wrong.')
+                }
+            })
+        }
+        
     }))
 })()
 
+document.querySelector('#print-quiz').onclick = () => {
+    let questions = document.querySelector('.questions')
+    html2pdf(questions, {filename: 'questions.pdf'})
+}
 
+document.querySelector('#print-answers').onclick = () => {
+    let answers = document.querySelectorAll('.correct-answer')
+    let html = '<ol>'
+    answers.forEach((answer) => {
+        html += `<li>${answer.innerText}</li>`
+    })
+    html += '</ol>'
+    html2pdf(html, {filename: 'answers.pdf'})
+}
 
