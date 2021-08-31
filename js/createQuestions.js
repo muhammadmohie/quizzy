add6a137a22769b47f45e132a4be52b2c1f09189
Adham Salama <adhamsalama@zohomail.com>
2021-08-31 15:37:31 +0200

    Fix displaying answer character

--- js/createQuestions.js
+++ js/createQuestions.js
@@ -1,19 +1,23 @@
 function createQuestion(title, answers, correctAnswer) {
     let html = `<div><li>
         <div class="question"><div class = "question-title">
-            <h2  "class="question-title" contenteditable="false" onfocusout ="upDArrayT(this)">${title}</h2><div class = "title-buttons"><button class = "answer-edit" onclick="editTitle(this);"><i class=" fas fa-edit" data-html2canvas-ignore="true"></i></button><button class ="trash-can" onclick="removeQ(this);"><i class="fas fa-trash" data-html2canvas-ignore="true"></i></button></div></div>`
+            <h2  "class="question-title" contenteditable="false" onfocusout ="upDArrayT(this)">${escapeHtml(title)}</h2><div class = "title-buttons"><button class = "answer-edit" onclick="editTitle(this);"><i class=" fas fa-edit" data-html2canvas-ignore="true"></i></button><button class ="trash-can" onclick="removeQ(this);"><i class="fas fa-trash" data-html2canvas-ignore="true"></i></button></div></div>`
 
     html += '<ol class="answer-list">'
     for(let answer of answers) {
         if (answer)
-        html += `<div class="answer-row-cont"><li contenteditable="false"  onfocusout ="upDArrayA(this)">${answer}</li><button class = "answer-edit" onclick="editA(this)"; ><i class="fas fa-edit" data-html2canvas-ignore="true"></i></button></div>`
+        html += `<div class="answer-row-cont"><li contenteditable="false"  onfocusout ="upDArrayA(this)">${escapeHtml(answer)}</li><button class = "answer-edit" onclick="editA(this)"; ><i class="fas fa-edit" data-html2canvas-ignore="true"></i></button></div>`
 
     }
     html += '</ol>'
     html += `<div class="correct-answer-div" data-html2canvas-ignore="true">
-        correct answer: <span class="correct-answer">${String(correctAnswer)}</span>
+        correct answer: <span class="correct-answer">${String.fromCharCode(65 + correctAnswer)}</span>
         </div>`
     html += '</div>'
     html += '</li></div>'
     return html
+}
+
+function escapeHtml(str) {
+    return str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
 }
